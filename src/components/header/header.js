d093dd884537fd46ada96c20f3b11b5f8a74efcc
ravivalluri <ravitejavalluri@gmail.com>
2019-04-18 10:32:12 -0500

    Initial Commit

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,25 +4,33 @@ import headerStyles from '../../css/header.css';
 export default class Header extends React.Component {
 
     render() {
-        return(
-            <div>
-                <nav className={headerStyles.nav_bar}>
-                    <ul className={headerStyles.nav_left}>
-                        <li>
-                            Payslip Generator
-                        </li>
-                    </ul>
-                    <ul className={headerStyles.nav_right}>
-                        <li>
-                            <span>Created by &nbsp;&nbsp;
-                                <a href="https://www.linkedin.com/in/venkata-sanikommu/" target="_blank" rel='noopener noreferrer'>
-                                    Venkata Reddy
-                                </a>
-                            </span>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+        return ( <
+            div >
+            <
+            nav className = {
+                headerStyles.nav_bar
+            } >
+            <
+            ul className = {
+                headerStyles.nav_left
+            } >
+            <
+            li >
+            PayStub Calculator <
+            /li> < /
+            ul > <
+            ul className = {
+                headerStyles.nav_right
+            } >
+            <
+            li >
+            <
+            span > Based on your inputs <
+            /span> < /
+            li > <
+            /ul> < /
+            nav > <
+            /div>
         )
     }
 }
